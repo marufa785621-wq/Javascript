71b997ed40e0b6ddc3c7cc6f078f2a3a07654ef0
Marufa Begum <marufa785621@gmail.com>
2026-09-22 16:59:07 +0530

    task

---
+++ task.js
@@ -0,0 +1,19 @@
+let studentName="Marufa";
+let age=18;
+var course="Bsc Data science";
+var collegeName="SITM";
+let marks=80;
+const birthyear=2008;
+const city="Golaghat";
+var semester="1st semester";
+const favouritesubject="English";
+
+console.log("Student Name:",studentName)
+console.log("Age:",age)
+console.log("Course Name:",course)
+console.log("College Name:",collegeName)
+console.log("Marks:",marks)
+console.log("Birth Year:",birthyear)
+console.log("City:",city)
+console.log("Semester:",semester)
+console.log("Favourite Subject:",favouritesubject)
